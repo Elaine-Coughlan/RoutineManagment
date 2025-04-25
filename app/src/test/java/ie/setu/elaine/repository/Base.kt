1928d44ac6c0c55f94a05515bede7d9ef0be09df
Elaine Coughlan <20067964@mail.wit.ie>
2025-04-25 19:39:56 +0100

    New Testing added (2)

---
+++ app/src/test/java/ie/setu/elaine/repository/Base.kt
@@ -0,0 +1,35 @@
+package ie.setu.elaine.repository
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.rules.TestRule
+import org.mockito.MockitoAnnotations
+
+@ExperimentalCoroutinesApi
+abstract class Base {
+    // Use InstantTaskExecutorRule to make LiveData work in tests
+    @get:Rule
+    val instantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    // Test dispatcher to control the execution of coroutines
+    protected val testDispatcher = TestCoroutineDispatcher()
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+}
